c6471b3656e9aaef9f1838ec102df8a41e61ba2a
John Ko <johnko426@gmail.com>
2016-05-20 19:53:41 -0700

    added stuff

---
+++ src/Customer.java
@@ -0,0 +1,11 @@
+
+public class Customer {
+	private String id;
+	private String first_name;
+	private String last_name;
+	private String address;
+	private String city;
+	private String state;
+	private String zip;
+	
+}
