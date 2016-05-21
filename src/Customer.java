f9c4985444d267731054c54fcd27222f99248e7b
Sophia Chan <sophiawc@uci.edu>
2016-05-20 20:00:11 -0700

    added getters and setters for Customer.java

--- src/Customer.java
+++ src/Customer.java
@@ -1,11 +1,60 @@
 
 public class Customer {
 	private String id;
-	private String first_name;
-	private String last_name;
+	private String firstName;
+	private String lastName;
 	private String address;
 	private String city;
 	private String state;
 	private String zip;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String newId) {
+		this.id = newId;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String newFirstName) {
+		this.firstName = newFirstName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String newLastName) {
+		this.lastName = newLastName;
+	}
+	
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String newAddress) {
+		this.address = newAddress;
+	}
+	
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String newCity) {
+		this.city = newCity;
+	}
+	
+	public String getState() {
+		return state;
+	}
+	public void setState(String newState) {
+		this.state = newState;
+	}
+	
+	public String getZip() {
+		return zip;
+	}
+	public void setZip(String newZip) {
+		this.zip = newZip;
+	}
 	
 }
