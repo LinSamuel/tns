6efd4d028d722f6dd116c28a1f2f774923405fa9
John Ko <johnko426@gmail.com>
2016-05-27 00:06:34 -0700

    Added a small polling script that polls via xhr

---
+++ WebContent/js/polling.js
@@ -0,0 +1,24 @@
+/**
+ * poll
+ * polls the currentviewnumservlet in {interval} number milliseconds
+ * 
+ * usage:
+ * 
+ * // on document ready
+ * <script src="/js/polling.js"></script>
+ * <script>
+ * poll(1);
+ * </script>
+ * 
+ * @param id {int} product id
+ * @param interval {int} milliseconds
+ */
+function poll(id, interval) {
+	var url = "/CurrentViewNumServlet?productID=" + id;
+	var xhr = new XMLHttpRequest();
+	
+	window.setInterval(function() {
+		xhr.open('GET', url, true);
+		xhr.send();
+	}, interval);
+}
