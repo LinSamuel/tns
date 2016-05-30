e53b64d0b2998aa7188e127cc0db9c7b561df594
John Ko <johnko426@gmail.com>
2016-05-29 21:34:05 -0700

    Removed Config class

--- src/tns/Config.java
+++
@@ -1,7 +0,0 @@
-package tns;
-public class Config {
-	public static final String address = "localhost";
-	public static final String dbname = "tnsdb";
-	public static final String userName = "root";
-	public static final String passWord = "myrootpw";
-}
